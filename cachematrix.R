ec1774bf611b28e0d3a5660f5f9fa20f1a1ac46d
ddali <nurminen.ville@gmail.com>
2015-06-21 23:59:02 +0300

    Revert "Update cachematrix.R"
    This reverts commit 355cc1bcf0fa6e8180c092aa661584789141e785.

--- cachematrix.R
+++ cachematrix.R
@@ -4,11 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
- #creates a matrix for the assignment, with two rows and four columns to, this is then filled
- #from 1 to 8 by columns (default setting)
- 
- matrix(1:8, nrow=2) 
- 
+testi
 }
 
 
@@ -16,5 +12,4 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        solve(makeCacheMatrix)
 }
